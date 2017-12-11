9d55087f178e209deb54902cb7fc96f827ccb0fd
Diego Imbus <dsn.imbus01@gmail.com>
2017-12-11 14:51:10 -0500

    imagenes de crossfit

--- app/src/main/java/unicauca/movil/peliculas/util/Data.kt
+++ app/src/main/java/unicauca/movil/peliculas/util/Data.kt
@@ -5,26 +5,33 @@ import unicauca.movil.peliculas.models.Pelicula
 object Data {
 
     var peliculas: MutableList<Pelicula> = mutableListOf(
-            Pelicula("Wonder Woman", "210 min", "30 de Mayo",
-                    "http://static.dnaindia.com/sites/default/files/2017/03/11/555566-wonder-woman-poster.jpg",
+            Pelicula("Clean & Jerk", "210 min", "30 de Mayo",
+                    "http://joangallardo.es/wp-content/uploads/2016/10/Split-jerk.jpg",
                     "Diana, una princesa guerrera perteneciente al pueblo de las amazonas, fue criada en una isla protegida y entrenada para ser una luchadora invencible. Después de que un piloto estadounidense, Steve Trevor, se estrellase en dicha isla, es salvado por la princesa, a quien le relata sobre el desarrollo de una guerra mundial que está sucediendo. Entonces, Diana sale de su hogar para tratar de detener la guerra."),
 
-            Pelicula("Alien Covenan", "220 min", "18 de Mayo",
-                    "https://www.diariodemorelos.com/noticias/sites/default/files/field/image/alien-covenant-diariodemorelos.jpg",
+            Pelicula("Snatch", "220 min", "18 de Mayo",
+                    "http://www.cuttystrength.com/wp-content/uploads/2013/10/womandeadlift6.jpg",
                     "En 2104, la nave colonial Covenant, con 2000 colonos y 1400 embriones a bordo, se dirige al remoto planeta Origae-6, vigilados por el androide Walter. Una onda de radiación neutrónica daña la nave, matando al capitán y despertando a la tripulación. Mientras reparan los daños, la tripulación detecta una señal de radio de origen humano procedente de un planeta cercano que aparentemente no tiene vida. En contra del criterio de Daniels, la experta en terraformación, el capitán Oram, oficial ejecutivo de la nave, decide investigar."),
 
-            Pelicula("Justice League", "240 min", "16 de Noviembre",
-                    "http://www.cheatsheet.com/wp-content/uploads/2016/07/Justice-League-first-image.jpg",
+            Pelicula("Cluster", "240 min", "16 de Noviembre",
+                    "http://crossfitcvi.com/content/wp-content/uploads/2014/11/Crossfit-CVI-Edwin-Thruster.jpg",
                     "Alimentado por su restaurada fe en la humanidad e inspirado por el acto desinteresado de Superman, Bruce Wayne recluta la ayuda de su recién encontrada aliada, Diana Prince, para enfrentarse a un enemigo aún mayor. Juntos, Batman y Wonder Woman trabajan deprisa para encontrar y reclutar un equipo de metahumanos para plantar cara a esta nueva amenaza creciente. Pero pese a la formación de esta liga de héroes sin precedentes – Batman, Wonder Woman, Aquaman, Cyborg y Flash, quizás sea demasiado tarde para salvar el planeta de un asalto de proporciones catastróficas."),
 
-            Pelicula("La Momia", "190 min", "08 de Junio",
-                    "http://www.carlost.net/wp-content/uploads/2016/12/La-Momia-2017-Trailer-Subtitulado-Carlost.jpg",
+            Pelicula("Deadlift", "240 min", "16 de Noviembre",
+                    "https://games-assets.crossfit.com/deadliftbrooke.jpg",
+                    "Alimentado por su restaurada fe en la humanidad e inspirado por el acto desinteresado de Superman, Bruce Wayne recluta la ayuda de su recién encontrada aliada, Diana Prince, para enfrentarse a un enemigo aún mayor. Juntos, Batman y Wonder Woman trabajan deprisa para encontrar y reclutar un equipo de metahumanos para plantar cara a esta nueva amenaza creciente. Pero pese a la formación de esta liga de héroes sin precedentes – Batman, Wonder Woman, Aquaman, Cyborg y Flash, quizás sea demasiado tarde para salvar el planeta de un asalto de proporciones catastróficas."),
+
+            Pelicula("Back Squat", "190 min", "08 de Junio",
+                    "https://i.ytimg.com/vi/ym1-lIlN4wc/maxresdefault.jpg",
                     "Tras encontrar y desenterrar a una momia perteneciente a una antigua princesa egipcia (Sofia Boutella) cuyo destino le fue arrebatado injustamente, un grupo de comandos militares trasladan la momia a Londres. Sin embargo, la momia despierta en mitad del vuelo, desencadenando el caos y comenzando su venganza."),
 
-            Pelicula("Spiderman", "190 min", "13 de Julio",
-                    "http://sm.ign.com/ign_latam/screenshot/default/spidermanhomecomingposter_u8zq.jpg",
-                    "Después de los acontecimientos de Capitán América: Civil War, Peter Parker, con la ayuda de su mentor Tony Stark, trata de equilibrar su vida como un estudiante de secundaria ordinaria en Queens, Nueva York con su lucha contra el crimen como su superhéroe alter ego Spider-Man mientras una nueva amenaza, el Buitre, aparece.")
+            Pelicula("Front Squat", "190 min", "13 de Julio",
+                    "http://nutrisportline.com/blog/wp-content/uploads/2016/09/Clean-and-Jerk-para-ganar-m%C3%A1s-fuerza-y-potencia-crossfit-alicante-suplementos-deportivos-tienda-suplementos-alicante-nutrisportline-suplementos-fitness-a-lo-big-hh-e1473447201469.jpg",
+                    "Después de los acontecimientos de Capitán América: Civil War, Peter Parker, con la ayuda de su mentor Tony Stark, trata de equilibrar su vida como un estudiante de secundaria ordinaria en Queens, Nueva York con su lucha contra el crimen como su superhéroe alter ego Spider-Man mientras una nueva amenaza, el Buitre, aparece."),
 
+            Pelicula("Overhead Squat", "190 min", "13 de Julio",
+                    "http://www.tabatatimes.com/wp-content/uploads/2013/05/MG_0270-copy.jpg",
+                    "Después de los acontecimientos de Capitán América: Civil War, Peter Parker, con la ayuda de su mentor Tony Stark, trata de equilibrar su vida como un estudiante de secundaria ordinaria en Queens, Nueva York con su lucha contra el crimen como su superhéroe alter ego Spider-Man mientras una nueva amenaza, el Buitre, aparece.")
     )
 
 }
